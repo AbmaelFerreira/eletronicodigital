3c1fa6ce809cea554ed3a0467af5acad048fcfdd
AbmaelFerreira <abmael_ninha@hotmail.com>
2019-11-22 22:54:57 -0400

    Criando o projeto

---
+++ src/main/java/com/vendas/EletronicodigitalApplication.java
@@ -0,0 +1,13 @@
+package com.vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletronicodigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletronicodigitalApplication.class, args);
+	}
+
+}
